db4263297761672ff659415dfd7a858ca0a650fb
Alejanro001 <53438345+Alejanro001@users.noreply.github.com>
2019-10-15 09:12:15 -0500

    first commit

---
+++ Reservation.UI.MVC/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace Reservation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
