c6a494a79e65307e26d0e3f522fa15a9dde8e534
Alejanro001 <53438345+Alejanro001@users.noreply.github.com>
2019-10-15 22:27:32 -0500

    commit -end of day Tuesday

---
+++ ClassLibrary1/Metadata/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClassLibrary1.Metadata
+{
+    public class AspNetUser
+    {
+
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PasswordHash { get; set; }
+        public string SecurityStamp { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public Nullable<System.DateTime> LockoutEndDateUtc { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public int AccessFailedCount { get; set; }
+        public string UserName { get; set; }
+    }
+    public class Location
+    {
+        public int LocationID { get; set; }
+        [Required(ErrorMessage = "While house calls are on the table for possible future endevors, we do not currently really do that. Please select a location.")]
+        public string LocationName { get; set; }
+        [Display(Name = "Address")]
+        [Required(ErrorMessage = "Please enter the street address.")]
+        public string Address { get; set; }
+        [Display(Name = "City")]
+        [Required(ErrorMessage = "Please enter the city.")]
+        public string City { get; set; }
+        [Display(Name = "State")]
+        [Required(ErrorMessage = "Please enter the two letter postal code.")]
+        public string State { get; set; }
+        [Display(Name = "Zip code")]
+        [Required(ErrorMessage="Please enter the zip code")]
+        public string ZipCode { get; set; }
+        [Range(1, int.MaxValue)]
+        public byte ReservationLimit { get; set; }
+    }
+    public class OwnerAsset
+    {   //TODO - Finish adding metadata
+        public int OwnerAssetID { get; set; }
+        public string AssetName { get; set; }
+        public string OwnerID { get; set; }
+        public string AssetPhoto { get; set; }
+        public string SpecialNotes { get; set; }
+        public bool IsActive { get; set; }
+        public System.DateTime DateAdded { get; set; }
+    }
+    public class Reservation
+    {
+        public int ReservationID { get; set; }
+        public int LocationID { get; set; }
+        public System.DateTime ReservationDate { get; set; }
+        public int OwnerAssetID { get; set; }
+
+    }
+    public class UserDetail
+    {
+        public string UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
